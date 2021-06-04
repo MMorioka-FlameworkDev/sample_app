6bd9e19fe5f86239279f7c9c3d647f4663486798
MMorioka <a2140mm@aiit.ac.jp>
2021-06-04 10:23:11 +0000

    Add password reset

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,14 +1,19 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+# Preview all emails at https://63edcdd58cbf4da59d9a7a25515c6a8e.vfs.cloud9.us-east-1.amazonaws.com/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
+  # Preview this email at
+  # https://63edcdd58cbf4da59d9a7a25515c6a8e.vfs.cloud9.us-east-1.amazonaws.com/rails/mailers/user_mailer/account_activation
   def account_activation
-    UserMailer.account_activation
+    user = User.first
+    user.activation_token = User.new_token
+    UserMailer.account_activation(user)
   end
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
+  # Preview this email at
+  # https://63edcdd58cbf4da59d9a7a25515c6a8e.vfs.cloud9.us-east-1.amazonaws.com/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
-
 end
